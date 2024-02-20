e3a61e3796c3a002ea5435e141e791be75efb8d0
MarcosPerez16 <marcos.perez06@yahoo.com>
2024-02-20 09:45:22 -0800

    created client pg, tables and seeded, express app, and listening on server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,42 @@
 // imports here for express and pg
-
+const express = require("express");
+const app = express();
+const path = require("path");
+const pg = require("pg");
+const client = new pg.Client(
+  process.env.DATABASE_URL || "postgres://localhost/acme_hr_db"
+);
 // static routes here (you only need these for deployment)
 
 // app routes here
 
 // create your init function
+const init = async () => {
+  await client.connect();
+
+  const SQL = `
+        DROP TABLE IF EXISTS employees;
+        CREATE TABLE employees(
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(50) NOT NULL,
+            is_admin BOOLEAN DEFAULT FALSE
+        );
+
+        INSERT INTO employees(name, is_admin) VALUES('John Doe', true)
+        INSERT INTO employees(name, is_admin) VALUES('Jane Smith', false)
+        INSERT INTO employees(name, is_admin) VALUES('Bob Johnson', false)
+
+
+    
+    
+    `;
+  await client.query(SQL);
+  console.log("Tables created and data seeded successfully");
+
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => console.log(`Listening on port ${port}`));
+};
 
 // init function invocation
+
+init();
